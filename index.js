d9c661831004ba9c0298d2398444d72f8384f316
wyc <wjunjobs@outlook.com>
2025-08-29 15:27:03 +0800

    删除自动写入 tracking code，通过文件自己配置

--- index.js
+++ index.js
@@ -80,16 +80,6 @@ hexo.on("deployBefore", async function (post) {
     }
 })
 
-//insert webpushr tracking code
-hexo.extend.filter.register('after_render:html', data => {
-    var payload = `(function (w, d, s, id) {
-            if (typeof (w.webpushr) !== 'undefined') return; w.webpushr = w.webpushr || function () { (w.webpushr.q = w.webpushr.q || []).push(arguments) }; var js, fjs = d.getElementsByTagName(s)[0]; js = d.createElement(s); js.id = id; js.async = 1; js.src = "https://cdn.webpushr.com/app.min.js";fjs.parentNode.appendChild(js);}(window, document, 'script', 'webpushr-jssdk'));webpushr('setup', { 'key': '${hexo.config.webPushNotification.trackingCode}' });`
-
-    // return data.replace(/<body>(?!<\/body>).+?<\/body>/s, str => str.replace('</body>', "<script>"+decodeURI(payload)+"</script></body>"));
-    return data.replace(/<body.+?>(?!<\/body>).+?<\/body>/s, str => str.replace('</body>', "<script>" + decodeURI(payload) + "</script></body>"));
-
-});
-
 //insert webpushr-sw.js to web root dir
 hexo.on('generateAfter', async function (post) {
     fs.writeFile('public/webpushr-sw.js', "importScripts('https://cdn.webpushr.com/sw-server.min.js');", function (err, data) { hexo.log.info("Generated: webpushr-sw.js") })
